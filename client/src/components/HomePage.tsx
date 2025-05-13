31eb18abfd65227722f7a16b54b6440e9b7a2c11
arnold-utama <arnoldutama3@gmail.com>
2025-05-13 14:02:24 +0700

    feat: enhance pagination functionality and vehicle display in HomePage component

--- client/src/components/HomePage.tsx
+++ client/src/components/HomePage.tsx
@@ -40,9 +40,17 @@ export default function HomePage() {
   });
 
   const [page, setPage] = useState<page>({
-    limit: 10,
+    limit: 5,
     offset: 0,
   });
+
+  function getTotalPage() {
+    return Math.ceil(
+      parseInt(vehicleResponse.links.last.split("page[offset]=")[1]) /
+        page.limit +
+        1
+    );
+  }
 
   useEffect(() => {
     async function fetchVehicles() {
@@ -56,16 +64,67 @@ export default function HomePage() {
       }
     }
     fetchVehicles();
-  }, []);
+  }, [page]);
+
+  function handlePrevPage() {
+    if (vehicleResponse.links.prev) {
+      setPage((prevPage) => ({
+        ...prevPage,
+        offset: prevPage.offset - prevPage.limit,
+      }));
+    }
+  }
+
+  function handleNextPage() {
+    if (vehicleResponse.links.next) {
+      setPage((prevPage) => ({
+        ...prevPage,
+        offset: prevPage.offset + prevPage.limit,
+      }));
+    }
+  }
 
   return (
     <div className="w-screen p-10">
       <h1 className="text-4xl font-bold mb-4">Vehicle List</h1>
-      <div className="flex flex-wrap w-full gap-4">
+      <div className="flex flex-wrap w-full gap-4 mb-5">
         {vehicleResponse.data.map((vehicle) => (
           <VehicleCard key={vehicle.id} vehicle={vehicle} />
         ))}
       </div>
+      <label>Data per page: </label>
+      <select
+        value={page.limit}
+        className="select w-auto"
+        onChange={(e) => setPage({ limit: Number(e.target.value), offset: 0 })}
+      >
+        <option>5</option>
+        <option>10</option>
+        <option>15</option>
+      </select>
+      <p>
+        Showing {page.offset + 1} to {page.offset + page.limit} of {page.limit * getTotalPage()} data
+      </p>
+      <div className="join">
+        <button
+          className="join-item btn"
+          onClick={handlePrevPage}
+          disabled={!vehicleResponse.links.prev}
+        >
+          «
+        </button>
+        <button className="join-item btn">
+          Page {Math.floor(page.offset / page.limit) + 1}
+        </button>
+        <button
+          className="join-item btn"
+          onClick={handleNextPage}
+          disabled={!vehicleResponse.links.next}
+        >
+          »
+        </button>
+      </div>
+      {vehicleResponse.links.last && <p>Total Page: {getTotalPage()}</p>}
     </div>
   );
 }
